73df202c239e1aa38e2f3bab049a8f1ac37b2ab8
Anna <anyakeba@gmail.com>
2022-08-26 13:51:09 +0300

    main, printMessage, printSum methods added

---
+++ src/com/test/Main.java
@@ -0,0 +1,21 @@
+package com.test;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        printMessage("Summa a + b =");
+        printSum(2, 6);
+
+    }
+
+        public static void printMessage (String s){
+            System.out.println(s);
+        }
+
+        public static void printSum (int a, int b){
+            System.out.println(a + b);
+
+    }
+}
+
